2b63134225c4bad4a29e9d5c93134fcd73ed86c8
Chris Olszewski <pdqman@gmail.com>
2014-09-06 17:23:43 -0500

    now actually ready for heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 8000;
+var pageCache = {};
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(port);
